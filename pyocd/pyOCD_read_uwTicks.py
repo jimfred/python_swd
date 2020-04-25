be64973532998396ec2ff12564c8a84932413dbb
Jim Frederick <jimfred@jimfred.org>
2020-04-24 21:27:00 -0400

    Read live data from an STM32F4 eval board
    Simple example program that reads live data from an STM32F4 eval board using its little ST-Link interface.

---
+++ pyocd/pyOCD_read_uwTicks.py
@@ -0,0 +1,30 @@
+# Use pyOCD to read data via an ST-Link from a microcontroller.
+# https://github.com/mbedmicro/pyOCD/blob/master/docs/api_examples.md
+
+from pyocd.core.helpers import ConnectHelper  # for ST-Link connection.
+from pyocd.debug.elf.symbols import ELFSymbolProvider  # for ELF file symbol lookup.
+import time # for sleep.
+
+session = ConnectHelper.session_with_chosen_probe(
+    return_first=True,      # Assuming that 1 board is attached. Or, we just use the first one.
+    connect_mode='attach')  # This will keep the target running. default is 'halt' in which case target.resume() should be called.
+
+session.open()
+
+target = session.board.target
+
+target.elf = "./Blink.elf" # Set ELF file on target.
+
+# Look up address of "uwTick", the millisecond counter in firmware.
+provider = ELFSymbolProvider(target.elf)
+tick_addr = provider.get_symbol_value("uwTick")
+print("uwTick at address: 0x{0:08X}".format(tick_addr))
+
+# Read uwTick and expect it to return a changing tick count.
+# It should change roughly 1000 msec each time by virtue of the sleep(1).
+for i in range(1, 10):
+    val = target.read32(tick_addr)
+    print(val)
+    time.sleep(1)
+
+
